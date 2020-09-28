43ca5b54b32d16bfcf2bb38654f7d8b5965a940b
ekatarzyna <ekate.uk@gmail.com>
2020-09-28 22:11:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#fav_number=input("What is your favourite number? ")
+#float(fav_number)
+
+def joke():
+
+ jokemenu = int(input("What is your favourite number from 1-10? "))
+
+ if jokemenu == 1:
+  print("""Q: How do you stay warm in an empty room?
+A: Go stand in the corner — it’s always 90 degrees there.""")
+ elif jokemenu == 2:
+   print("JOKE2")
+ elif jokemenu == 3:
+   print("JOKE3")
+ elif jokemenu == 4:
+   print("JOKE4")
+ elif jokemenu == 5:
+   print("JOKE5")
+ elif jokemenu == 6:
+   print("JOKE6")
+ elif jokemenu == 7:
+   print("JOKE7")
+ elif jokemenu == 8:
+   print("JOKE8")
+ elif jokemenu == 9:
+   print("JOKE9")
+ elif jokemenu == 10:
+   print("JOKE10")
+ else:
+  print("Please type the number")
+
+
+joke()
